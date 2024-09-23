a47c866667467c223ab9f9c9944b695b7eb758d2
Neha27Gupta5 <124888471+Neha27Gupta5@users.noreply.github.com>
2024-09-23 14:49:00 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Rat in a Maze Problem - I/rat-in-a-maze-problem-i.cpp
+++ Difficulty: Medium/Rat in a Maze Problem - I/rat-in-a-maze-problem-i.cpp
@@ -10,46 +10,9 @@ using namespace std;
 
 class Solution {
   public:
-//     void solve(vector<vector<int>>& mat, vector<string>& ans, vector<vector<bool>>& vis, string temp, int row, int col, int n, int m) {
+    void solve(vector<vector<int>>& mat, vector<string>& ans, vector<vector<bool>>& vis, string temp, int row, int col, int n, int m) {
    
-//     if (row == n-1 && col == m-1) {
-//         ans.push_back(temp);
-//         return;
-//     }
-
-   
-//     if (row < 0 || row >= n || col < 0 || col >= m || mat[row][col] == 0 || vis[row][col]) {
-//         return;
-//     }
-
-    
-//     vis[row][col] = true;
-
-    
-//     solve(mat, ans, vis, temp+'U', row-1, col, n, m);
-//     solve(mat, ans, vis, temp+'D', row+1, col, n, m);
-//   solve(mat, ans, vis, temp+'L', row, col-1, n, m); 
-//  solve(mat, ans, vis, temp+'R', row, col+1, n, m); 
-
-  
-//     vis[row][col] = false;
-// }
-
-// vector<string> findPath(vector<vector<int>>& mat) {
-//     vector<string> ans;
-//     int n = mat.size();
-//     int m = mat[0].size();
-//     vector<vector<bool>> visited(n, vector<bool>(m, false));
-//     string temp = "";
-
-    
-//     solve(mat, ans, visited, temp, 0, 0, n, m);
-
-//     return ans;
-// }
-void solve(vector<vector<int>>& mat, vector<string>& ans, vector<vector<bool>>& vis, string& temp, int row, int col, int n, int m) {
-    // Base condition: If reached bottom-right corner, add the current path to the result
-     if (row < 0 || row >= n || col < 0 || col >= m || mat[row][col] == 0 || vis[row][col]) {
+    if (row < 0 || row >= n || col < 0 || col >= m || mat[row][col] == 0 || vis[row][col]) {
         return;
     }
     
@@ -58,19 +21,19 @@ void solve(vector<vector<int>>& mat, vector<string>& ans, vector<vector<bool>>& 
         return;
     }
 
-    // If out of bounds, cell is blocked, or already visited, return
-  
+   
+   
 
-    // Mark the cell as visited
+    
     vis[row][col] = true;
 
-    // Explore all directions: Up, Down, Left, Right
-    temp.push_back('U'); solve(mat, ans, vis, temp, row-1, col, n, m); temp.pop_back();
-    temp.push_back('D'); solve(mat, ans, vis, temp, row+1, col, n, m); temp.pop_back();
-    temp.push_back('L'); solve(mat, ans, vis, temp, row, col-1, n, m); temp.pop_back();
-    temp.push_back('R'); solve(mat, ans, vis, temp, row, col+1, n, m); temp.pop_back();
+    
+    solve(mat, ans, vis, temp+'U', row-1, col, n, m);
+    solve(mat, ans, vis, temp+'D', row+1, col, n, m);
+  solve(mat, ans, vis, temp+'L', row, col-1, n, m); 
+ solve(mat, ans, vis, temp+'R', row, col+1, n, m); 
 
-    // Backtrack: Unmark the cell as visited
+  
     vis[row][col] = false;
 }
 
@@ -81,12 +44,11 @@ vector<string> findPath(vector<vector<int>>& mat) {
     vector<vector<bool>> visited(n, vector<bool>(m, false));
     string temp = "";
 
-    // Start solving from the top-left corner (0, 0)
+    
     solve(mat, ans, visited, temp, 0, 0, n, m);
 
     return ans;
 }
-
 
 };
 
